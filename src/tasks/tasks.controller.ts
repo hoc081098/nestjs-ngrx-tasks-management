f70db10ff6a6ab9d56ff372a7603de6d1ec7848d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-13 00:32:18 +0700

    add

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) { }
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string) {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+      @Param('id')  id: string,
+      @Body('status')  status: TaskStatus,
+  ) {
+    // done 21
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import * as uuidv1 from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const {title, description} = createTaskDto;
+
+    const task: Task = {
+      id: uuidv1(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks = [...this.tasks, task];
+    return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(t => t.id === id);
+  }
+
+  deleteTaskById(id: string) {
+    this.tasks = this.tasks.filter(t => t.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = {...this.getTaskById(id), status};
+    this.tasks = this.tasks.map(t => t.id === id ? task : t);
+    return task;
+  }
+}
