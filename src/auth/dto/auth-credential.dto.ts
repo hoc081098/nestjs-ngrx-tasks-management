d1f876922e0f0a52e82c286de5cda4a675d16c75
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-13 22:15:22 +0700

    Validation AuthCredentialsDto, password strength

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,9 +1,19 @@
-import { MinLength } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialDto {
-  @MinLength(3)
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
-  @MinLength(6)
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      {
+        message: 'Too weak password. Require minimum 8 characters, at least 1 letter, 1 number and 1 special character',
+      },
+  )
   password: string;
 }
