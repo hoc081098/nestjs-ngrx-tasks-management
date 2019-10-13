f53ae34fc2e721327ec76d87e8c2a37d1ae61300
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-13 19:35:41 +0700

    Connecting NestJS to a database using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+      TasksModule,
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '12345678',
+        database: 'task_management',
+        entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
