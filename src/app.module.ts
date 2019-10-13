8b0038e29f4f03466b4461e6871733a5a4fb2720
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-13 21:48:18 +0700

    Setting up AuthModule, User Entity and UserRepository

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,22 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-      TasksModule,
-      TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '12345678',
-        database: 'task_management',
-        entities: [join(__dirname, '**/*.entity{.ts,.js}')],
-        synchronize: true,
-      }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345678',
+      database: 'task_management',
+      entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+      synchronize: true,
+    }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
