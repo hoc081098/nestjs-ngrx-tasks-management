7041cb7a93c2535ff00ea373de00b15da26f9726
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-19 22:45:29 +0700

    Signing a JWT Token Upon Authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+      private readonly userRepository: UserRepository,
+      private readonly jwtService: JwtService,
+  ) {}
 
   signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.signUp(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<void> {
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
     const result = await this.userRepository.validateUser(authCredentialDto);
     if (result === 'NOT_EXISTS') {
       throw new UnauthorizedException('Not found user');
@@ -18,5 +23,10 @@ export class AuthService {
     if (result === 'WRONG_PASSWORD') {
       throw new UnauthorizedException('Incorrect password');
     }
+
+    const username = result.username;
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+    return { accessToken };
   }
 }
