2635733b2f23e444ef1b28ac7be03b83bcab5773
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-10-19 22:20:34 +0700

    Setting up the JWT Module and Passport.js

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'hoc081098',
+      signOptions: {
+        expiresIn: '3600',
+      },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
